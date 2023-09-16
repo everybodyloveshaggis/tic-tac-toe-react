5ba5c2dd7459df5d57fc08fe42bfb26d7185166e
Scott McMahon <everybodyloveshaggis@gmail.com>
2023-09-16 11:29:31 +0100

    checking for overwrites and winners disabling game

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-
 import { useState } from 'react';
+
+let isComplete = false;
 
 const generateBoard = (size) => {
   const newBoard = []
@@ -10,20 +11,20 @@ const generateBoard = (size) => {
 }
 
 const checkHorizontal = (board) => {
-  for(let row of board){
+  for (let row of board) {
     const rowSet = new Set(row)
-    if(rowSet.size === 1 && !rowSet.has(undefined)){
+    if (rowSet.size === 1 && !rowSet.has(undefined)) {
       return true
     }
   }
 }
 
-const rowsToColumns = (board) =>{
-  const newBoard=[]
-  let column =0
-  while(column < board.length){
-    const newRow =[]
-    for(let row=0; row < board.length; row++){
+const rowsToColumns = (board) => {
+  const newBoard = []
+  let column = 0
+  while (column < board.length) {
+    const newRow = []
+    for (let row = 0; row < board.length; row++) {
       newRow.push(board[row][column])
     }
     newBoard.push(newRow)
@@ -33,10 +34,10 @@ const rowsToColumns = (board) =>{
 }
 
 const diagonalToRow = (board) => {
-  const newBoard = [[],[]]
-  let increment=0
-  let decrement= board.length -1
-  while(increment < board.length){
+  const newBoard = [[], []]
+  let increment = 0
+  let decrement = board.length - 1
+  while (increment < board.length) {
     newBoard[0].push(board[increment][increment])
     newBoard[1].push(board[decrement][increment])
     increment++
@@ -47,66 +48,75 @@ const diagonalToRow = (board) => {
 
 const checkForWinner = (board) => {
   //horizontal
-  if(checkHorizontal(board)) {
+  if (checkHorizontal(board)) {
     return true
   }
   //vertical
-  if(checkHorizontal(rowsToColumns(board))) {
+  if (checkHorizontal(rowsToColumns(board))) {
     return true
   }
   //diagonal
-  if(checkHorizontal(diagonalToRow(board))) {
+  if (checkHorizontal(diagonalToRow(board))) {
     return true
   }
 }
 
-
-
 function App() {
-  const [board, setBoard] = useState(generateBoard(6))
+
+  const [board, setBoard] = useState(generateBoard(3))
   const [currPlayer, setCurrPlayer] = useState('x')
-  const handleClick = (row,col) => {
-    board[row][col] =currPlayer
-    setBoard([...board])
-    if(checkForWinner(board) ){
-      console.log("User " + currPlayer + " wins!")
+
+  const handleClick = (row, col) => {
+    if (!isComplete) {
+      //Here I am checking to make sure that the user is 
+      //clicking on a blank cell
+      //otherwise the click is not registered until they do.
+      if (!board[row][col]) {
+        board[row][col] = currPlayer
+        setBoard([...board])
+        if (checkForWinner(board)) {
+          isComplete = true;
+          console.log("User " + currPlayer + " wins!")
+        }
+        setCurrPlayer(currPlayer === 'x' ? 'y' : 'x')
+      }
     }
-    setCurrPlayer(currPlayer ==='x' ? 'y' : 'x')
   }
+
   return (
     <>
-    <div>
-      {board.map((row, r) => {
-        return (
-          <div
-            key={r}
-            style={{
-              display: 'flex',
-              border: '1px red solid',
-            }}
-          >
-            {row.map((cell, c) => {
-              return (
-                <div
-                  key={c}
-                  onClick={() => handleClick(r,c)}
-                  style={{
-                    border: '1px red solid',
-                    height: '50px',
-                    width: '50px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  {cell}
-                </div>
-              )
-            })}
-          </div>
-        )
-      })}
-    </div>
+      <div>
+        {board.map((row, r) => {
+          return (
+            <div
+              key={r}
+              style={{
+                display: 'flex',
+                border: '1px red solid',
+              }}
+            >
+              {row.map((cell, c) => {
+                return (
+                  <div
+                    key={c}
+                    onClick={() => handleClick(r, c)}
+                    style={{
+                      border: '1px red solid',
+                      height: '50px',
+                      width: '50px',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    {cell}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
